2edb6738b6e4f74ea20e0f70f18870c0545db773
Chuse <chasevillarroel@gmail.com>
2022-06-16 20:56:45 +0800

    moved kwarg parsing to its own method for readability

--- docket/main.py
+++ docket/main.py
@@ -8,6 +8,46 @@ from docket.conky import Conky
 
 class Docket:
     def __init__(self, **kwargs):
+        self._handle_kwargs(kwargs)
+
+        try:
+            self.refresh = float(self.conky.settings["refresh"])
+        except:
+            self.refresh = 5
+
+        self.log.info("Refresh period set to {}s".format(self.refresh))
+
+    def start(self):
+        try:
+            while True:
+                has_crossed_time_bound = False
+
+                today = today_()
+                if not today == self.yesterday:
+                    self.log.info("New day! Day: {} (iso)".format(today))
+                    self.schedule.update_day(today)
+                    has_crossed_time_bound =True
+                self.yesterday = today
+
+                while (len(self.schedule.time_bounds) > 0 and
+                        now_() > self.schedule.time_bounds[0]):
+                    crossed = self.schedule.time_bounds.pop(0)
+                    self.log.info("Crossed time bound: " + crossed)
+                    has_crossed_time_bound = True
+
+                if has_crossed_time_bound:
+                    self.schedule.update_status(now_())
+                    self.conky.update_config(self.schedule.day)
+
+                sleep(self.refresh)
+
+        except KeyboardInterrupt:
+            print("\ndocket: Received KeyboardInterrupt. Goodbye!")
+
+
+
+    # private methods
+    def _handle_kwargs(self, kwargs):
         if "conky_path" in kwargs.keys():
             conky_path = kwargs["conky_path"]
             self.conky = Conky(conky_path)
@@ -31,40 +71,6 @@ class Docket:
             self.log = Log(log_to_file, log_path)
         else:
             self.log = Log()
-
-        try:
-            self.refresh = float(self.conky.settings["refresh"])
-        except:
-            self.refresh = 5
-
-        self.log.info("Refresh period set to {}s".format(self.refresh))
-
-    def start(self):
-        try:
-            while True:
-                has_crossed_time_bound = False
-
-                today = today_()
-                if not today == self.yesterday:
-                    self.schedule.update_day(today)
-                    has_crossed_time_bound =True
-                self.yesterday = today
-
-                while (len(self.schedule.time_bounds) > 0 and
-                        now_() > self.schedule.time_bounds[0]):
-                    crossed = self.schedule.time_bounds.pop(0)
-                    self.log.info("Crossed time bound: " + crossed)
-                    has_crossed_time_bound = True
-
-                if has_crossed_time_bound:
-                    self.log.debug("Updating conky config...")
-                    self.schedule.update_status(now_())
-                    self.conky.update_config(self.schedule.day)
-
-                sleep(self.refresh)
-
-        except KeyboardInterrupt:
-            print("\ndocket: Received KeyboardInterrupt. Goodbye!")
 
 if __name__ == "__main__":
     docket = Docket()
