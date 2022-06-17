296fb6ff523916eb91ca500caf010f7853b0db1c
Chuse <chasevillarroel@gmail.com>
2022-06-17 20:32:55 +0800

    finalizing changes

--- start_all.sh
+++ start_all.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
-# Change this to your install path
+# change this to your install path
 path_to_docket="$HOME/Docket-lite"
 cd $path_to_docket
+
 x-terminal-emulator -e "python3 $path_to_docket/start.py" &
+
+# wait for docket to write to file
 sleep 1
-x-terminal-emulator -e "conky -c $path_to_docket/conky-docket.conf"
+
+x-terminal-emulator -e "conky -c $path_to_docket/conky-docket.conf" &
+
+x-terminal-emulator -e "conky -c $path_to_docket/conky-day.conf"
