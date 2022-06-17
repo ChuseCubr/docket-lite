70f9844b4456e538094ce600ce7b3e3cd1f44df3
Chuse <chasevillarroel@gmail.com>
2022-06-18 00:00:19 +0800

    comments

--- start_all.sh
+++ start_all.sh
@@ -3,10 +3,14 @@
 path_to_docket="$HOME/Docket-lite"
 cd $path_to_docket
 
+# run python script that will update the config
 x-terminal-emulator -e "python3 $path_to_docket/start.py" &
 
 # wait for docket to write to file
 sleep 1
 
+# actual schedule
 x-terminal-emulator -e "conky -c $path_to_docket/conky-docket.conf" &
+
+# week counter and weekday
 x-terminal-emulator -e "conky -c $path_to_docket/conky-day.conf"
